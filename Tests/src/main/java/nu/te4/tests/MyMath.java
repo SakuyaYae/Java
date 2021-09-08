9ca25111a27af2c17281d0ee0c308f7a56d3ff6f
SakuyaYae <darkreimu@outlook.com>
2021-09-08 14:52:40 +0200

    __

---
+++ Tests/src/main/java/nu/te4/tests/MyMath.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testarjunit;
+
+/**
+ *
+ * @author Daniel
+ */
+public class MyMath {
+    
+    public int add(String a, String b){       
+        int tal1, tal2; 
+        try {
+            tal1 = Integer.parseInt(a);
+            tal2 = Integer.parseInt(b);
+            return tal1+tal2;
+        } catch (Exception e) { //Vilket exceptions är det vi får här? kan förbättras.
+            throw new NumberFormatException("Du måste ha siffror");
+        }
+    }
+    
+    public boolean isEqual(int a, int b){
+        return a == b;
+    }
+    
+    public int difference(int a, int b){
+        
+        int res = 0;
+        if(a == 4){
+           res = a - b;
+           
+            
+        }
+        
+        if(b == 4){
+         res = b - a;
+        }    
+         
+        return res;
+    }
+    
+    public int difference(String a,String b){
+        
+        int res = 0;
+        
+        
+           int tal1, tal2; 
+        try {
+            tal1 = Integer.parseInt(a);
+            tal2 = Integer.parseInt(b);
+            
+            
+             
+        if(tal1 == 4){
+           res = tal1 - tal2;
+           
+            
+        }
+        
+        if(tal2 == 4){
+         res = tal2 - tal1;
+        }    
+         
+        return res;
+
+        } catch (Exception e) { //Vilket exceptions är det vi får här? kan förbättras.
+            throw new NumberFormatException("Du måste ha siffror");
+        }
+    }
+
+}
---
+++ Tests/src/main/java/nu/te4/tests/MyMath2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testarjunit;
+
+/**
+ *
+ * @author Daniel
+ */
+public class MyMath2 {
+    
+        
+    public double convertToFarenheit(double c){
+        if(c<-273.15){
+            throw new IllegalArgumentException("Temperature cant be lower than 0 Kelvin.");
+        }            
+         double f = 1.8*c + 32;
+        return f; 
+    }
+    
+    public int compareTo(int a, int b){
+        
+        //om a > b --> 1
+        if(a > b){
+        return 1;
+        }
+        //om a == b --> 0
+        else if(a == b){
+        return 0;
+        }
+        //om a < b --> -1
+        else{
+        return (-1);
+        }
+        
+    }
+    
+}
